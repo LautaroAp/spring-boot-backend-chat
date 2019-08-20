8932f872b9c5df9fb07415524668d80121b81674
LautaroAp <lautaro.ap@gmail.com>
2019-08-20 12:07:37 -0300

    historial con mongodb

--- src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
@@ -3,16 +3,25 @@ package com.bolsadeideas.springboot.backend.chat.controllers;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
+import com.bolsadeideas.springboot.backend.chat.models.services.ChatService;
 
 @Controller
 public class ChatController {
 	
 	private String[] colores = {"black", "gray", "blue", "red", "purple", "magenta", "orange"};
+	
+	@Autowired
+	private ChatService chatService;
+	
+	@Autowired
+	private SimpMessagingTemplate webSocket;
 	
 	/* Recibe el mensaje y le asigna la Fecha y el Texto */
 	@MessageMapping("/mensaje") // Para publicar
@@ -23,6 +32,8 @@ public class ChatController {
 		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
 			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
 			mensaje.setTexto("Nuevo usuario");
+		} else {
+			chatService.guardar(mensaje);
 		}
 								
 		return mensaje;
@@ -32,6 +43,11 @@ public class ChatController {
 	@SendTo("/chat/escribiendo")
 	public String estaEscribiendo(String username) {
 		return username.concat(" está escribiendo...");
+	}
+	
+	@MessageMapping("/historial")
+	public void historial(String clienteId){
+		webSocket.convertAndSend("/chat/historial/"+clienteId, chatService.obtenerUltimos10Mensajes());
 	}
 
 }
--- src/main/java/com/bolsadeideas/springboot/backend/chat/models/documents/Mensaje.java
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/models/documents/Mensaje.java
@@ -2,12 +2,19 @@ package com.bolsadeideas.springboot.backend.chat.models.documents;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="mensajes")
 public class Mensaje implements Serializable {
 
 	// ==========================================================
 	// ATRIBUTOS
 	// ==========================================================
 
+	@Id
+	private String id;
+	
 	private String texto;
 	private Long fecha;
 	private String username;
@@ -49,6 +56,12 @@ public class Mensaje implements Serializable {
 	}
 	public void setColor(String color) {
 		this.color = color;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
 	}
 	
 	
